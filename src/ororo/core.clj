bea5d14183e40e4582afbcb5aa343a817e880604
Anthony Grimes <i@raynes.me>
2011-10-21 01:42:50 -0500

    Added some documentation.

--- src/ororo/core.clj
+++ src/ororo/core.clj
@@ -1,3 +1,6 @@
+;; Ororo is a library for working with the wunderground API. The API is pretty simple and
+;; not too large, so it can mostly be done automatically. Some pieces are a bit inconsistent
+;; and are implemented differently.
 (ns ororo.core
   (:use [clojure.data.json :only [read-json pprint-json]]
         [clojure.string :only [join]])
@@ -5,7 +8,10 @@
 
 (def base-url "http://api.wunderground.com/api/")
 
-(defn- create-url [key features location]
+(defn- create-url
+  "Intelligently create a URL out of an API key, features (api calls), and
+   the query location."
+  [key features location]
   (str base-url key "/"
        (if (coll? features)
          (join "/" (map name features))
@@ -24,20 +30,31 @@
     (or (f m) (-> m :response :results))
     m))
 
-(defn api-call [key features location sift-fn]
+;; Ororo provides a lot of functions for working with the wunderground API by default.
+;; However, wunderground allows you to get more than one type of data in a single request.
+;; This cuts down on API requests one has to make to get a lot of information. For those
+;; situations, you can use this function to execute a request yourself.
+(defn api-call
+  "Make an API call out of a key, some features (as keywords, or just one keyword),
+   a location, and a 'sifting' function that will be applied to the resulting map."
+  [key features location sift-fn]
   (-> (create-url key features location)
       http/get
       :body
       read-json
       (sift sift-fn)))
 
-(defn fix-symbol [k]
-  (name k))
-
-(defn- api-fn [feature sift-fn]
+(defn- api-fn
+  "Create a function that makes an API call based on a bit of info."
+  [feature sift-fn]
   (fn [key location]
     (api-call key feature location sift-fn)))
 
+;; We're not going to generate these functions automatically, since that'd require
+;; making things unnecessarily complex with macro. We wouldn't gain much either way.
+;; Instead, we'll define each of our API functions separately using a convenient
+;; `api-fn` function and def. This is easier since we're on 1.3 and have inline
+;; docstrings in `def`.
 (def geolookup
   "Returns the city name, zip code/postal code, latitude-longitude coordinates and
    and nearby weather stations."
@@ -88,6 +105,7 @@
    low temperatures, a string text forecast, and the conditions."
   (api-fn :forecast7day :forecast))
 
+;; This one is implemented a bit differently because it requires a date.
 (defn history
   "Returns a summary of the observed weather for the specified date, which
    Should be a string of the format YYYYMMDD."
